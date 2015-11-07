5a4d4ab4f619366a26319f3a575870e600d5473e
Marifel Barbasa <mrbarbasa@gmail.com>
2015-11-06 20:28:31 -1000

    Add some game logic to make the sprites fall on repeat

--- public/js/game/main.js
+++ public/js/game/main.js
@@ -1,33 +1,62 @@
-var renderer = PIXI.autoDetectRenderer(800, 600,{backgroundColor : 0x1099bb});
+var renderer = PIXI.autoDetectRenderer(900, 600,{backgroundColor : 0x1099bb});
 document.body.appendChild(renderer.view);
 
 // create the root of the scene graph
 var stage = new PIXI.Container();
 
-// create a texture from an image path
-var texture = PIXI.Texture.fromImage('assets/bunny.png');
+var lane1 = new PIXI.Container();
+var lane2 = new PIXI.Container();
+var lane3 = new PIXI.Container();
 
-// create a new Sprite using the texture
-var bunny = new PIXI.Sprite(texture);
+// create a texture from an image path
+var hero1 = PIXI.Sprite.fromImage('assets/heroes/bunny.png');
+var hero2 = PIXI.Sprite.fromImage('assets/heroes/bunny.png');
+var hero3 = PIXI.Sprite.fromImage('assets/heroes/bunny.png');
 
 // center the sprite's anchor point
-bunny.anchor.x = 0.5;
-bunny.anchor.y = 0.5;
+hero1.anchor.x = 0.5;
+hero1.anchor.y = 0.5;
+hero2.anchor.x = 0.5;
+hero2.anchor.y = 0.5;
+hero3.anchor.x = 0.5;
+hero3.anchor.y = 0.5;
 
 // move the sprite to the center of the screen
-bunny.position.x = 200;
-bunny.position.y = 150;
+hero1.position.x = 150;
+hero1.position.y = 300;
+lane1.addChild(hero1);
 
-stage.addChild(bunny);
+hero2.position.x = 450;
+hero2.position.y = 300;
+lane2.addChild(hero2);
+
+hero3.position.x = 750;
+hero3.position.y = 300;
+lane3.addChild(hero3);
+
+stage.addChild(lane1);
+stage.addChild(lane2);
+stage.addChild(lane3);
 
 // start animating
 animate();
 function animate() {
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    // just for fun, let's rotate mr rabbit a little
-    bunny.rotation += 0.1;
+  hero1.position.y += 1;
+  hero2.position.y += 1;
+  hero3.position.y += 1;
 
-    // render the container
-    renderer.render(stage);
+  if (hero1.position.y === 600) {
+    hero1.position.y = 0;
+  }
+  if (hero2.position.y === 600) {
+    hero2.position.y = 0;
+  }
+  if (hero3.position.y === 600) {
+    hero3.position.y = 0;
+  }
+
+  // render the container
+  renderer.render(stage);
 }
