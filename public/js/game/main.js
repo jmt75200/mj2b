3a5b7c17c412b4272cee8ab5c8f84f1fe8f150be
Marifel Barbasa <mrbarbasa@gmail.com>
2015-11-07 02:01:22 -1000

    Create a rectangle using Pixi graphics for each zone

--- public/js/game/main.js
+++ public/js/game/main.js
@@ -6,7 +6,10 @@ Game.SETTINGS = {
   backgroundColor: 0x1099bb,
   canvasWidth: 800,
   canvasHeight: 600,
-  numLanes: 5
+  numLanes: 5,
+  numZonesPerLane: 8,
+  playerOneZoneColor: 0xEDEFF5,
+  playerTwoZoneColor: 0x4A565D
 };
 
 Game.renderer = PIXI.autoDetectRenderer(Game.SETTINGS.canvasWidth, Game.SETTINGS.canvasHeight,{
@@ -17,10 +20,36 @@ Game.init = function init(numLanes) {
   document.body.appendChild(Game.renderer.view);
 
   var numLanes = numLanes || Game.SETTINGS.numLanes;
+  var laneWidth = Game.SETTINGS.canvasWidth / numLanes;
+
+  var graphics = new PIXI.Graphics();
+  var zone1 = Game.SETTINGS.playerOneZoneColor;
+  var zone2 = Game.SETTINGS.playerTwoZoneColor;
+  var zoneHeight = Game.SETTINGS.canvasHeight / Game.SETTINGS.numZonesPerLane;
+  var zoneColor = null;
+  var zoneTemp = null;
+
+  for (var i = 0; i < numLanes; i++) {
+    for (var k = 0; k < Game.SETTINGS.numZonesPerLane; k++) {
+      if (k % 2 === 0) {
+        zoneColor = zone1;
+      } else {
+        zoneColor = zone2;
+      }
+
+      graphics.beginFill(zoneColor);
+      graphics.drawRect(i * laneWidth, k * zoneHeight, laneWidth, zoneHeight);
+    }
+
+    zoneTemp = zone1;
+    zone1 = zone2;
+    zone2 = zoneTemp;
+  }
+
+  Game.stage.addChild(graphics);
 
   var xStartingPos = 0;
   var yStartingPos = Game.SETTINGS.canvasHeight / 2;
-  var laneWidth = Game.SETTINGS.canvasWidth / numLanes;
 
   for (var i = 0; i < numLanes; i++) {
     var lane = new PIXI.Container();
